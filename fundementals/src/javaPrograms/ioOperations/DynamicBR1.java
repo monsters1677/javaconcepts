9141bf336a762e8ddda9f177ba2b401e92f3877e
monsters1677 <Ramana@9963434347>
2025-01-29 13:58:22 +0530

    Adding java program package to this project.

---
+++ fundementals/src/javaPrograms/ioOperations/DynamicBR1.java
@@ -0,0 +1,24 @@
+package fundementals.src.javaPrograms.ioOperations;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class DynamicBR1 {
+    public static void main(String[] args){
+        try(
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+        ){
+            System.out.print("Enter Data     : ");
+            String data = br.readLine();
+            System.out.print("Enter the same data again      : ");
+            int val = br.read();
+
+
+            System.out.println("readLine()  : " + data);
+            System.out.println("read()      : " + val+"["+(char)val+"]");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
